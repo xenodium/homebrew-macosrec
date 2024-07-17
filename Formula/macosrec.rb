f5bc6ccfb68047ecd41f26748da46f546906ae64
xenodium <me+gh@xenodium.com>
2024-07-17 11:44:39 +0100

    New release

--- Formula/macosrec.rb
+++ Formula/macosrec.rb
@@ -2,7 +2,7 @@ class Macosrec < Formula
   desc "Take screenshots or videos of macOS windows from the command line."
   homepage "https://github.com/xenodium/macosrec"
   url "https://github.com/xenodium/macosrec/archive/refs/tags/macosrec-0.7.2.tar.gz"
-  sha256 "2e74d62bd9390a7ac65455253860bd4e7b0083e7d27306b39a4da2e19fc130fb"
+  sha256 "72ad4fd1a341236f62dc6c646c95325cfe1ddf5f66fad02544af045c30dac115"
   license "GPL-3.0-or-later"
 
   depends_on :macos
