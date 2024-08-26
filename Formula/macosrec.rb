ab21e291ceae61f0696127c42d5cfa4e31675b2d
xenodium <me+gh@xenodium.com>
2024-08-26 21:31:25 +0100

    Fixing hash

--- Formula/macosrec.rb
+++ Formula/macosrec.rb
@@ -2,7 +2,7 @@ class Macosrec < Formula
   desc "Take screenshots or videos of macOS windows from the command line."
   homepage "https://github.com/xenodium/macosrec"
   url "https://github.com/xenodium/macosrec/archive/refs/tags/macosrec-0.8.1.tar.gz"
-  sha256 "712ef670fc8e1327c95317cb2fbc12a8a1c318bf9ea40e6d2b9d696c0fe45ca4"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "GPL-3.0-or-later"
 
   depends_on :macos
